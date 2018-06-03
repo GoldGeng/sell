0ba09f11475bb6ec69351c59f85822c2493950c8
GlodGeng <gengjin1987@126.com>
2018-06-03 23:01:49 +0800

    外卖项目1.0

---
+++ src/components/ratings.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="ratings">
+		ratings
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name: 'ratings',
+		data () {
+			return {
+				value: 0
+			}
+		}
+	}
+</script>
+
+<style type="text/css">
+	
+</style>
---
+++ src/components/sellers.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="sellers">
+		sellers
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name: 'sellers',
+		data () {
+			return {
+				value: 0
+			}
+		}
+	}
+</script>
+
+<style type="text/css">
+	
+</style>
